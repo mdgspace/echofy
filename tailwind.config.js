3dd191fad7df75113195fefdbb24109e7d9dbcbd
Aaditya-G <aadityagupta400@gmail.com>
2023-10-03 20:54:43 +0530

    feat: initializes tailwind css and adds basic chat layout

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        '16': 'repeat(16, minmax(0, 1fr))',
+      },
+      gridColumn: {
+        'span-13': 'span 13 / span 13',
+      },
+      gridRow: {
+        'span-10': 'span 10 / span 10',
+      },
+      colors: {
+        'bg-orange': '#F95131AB',
+      },
+    },
+    plugins: [],
+  }
+}
