5a528fbff3ae991b26766677be9d3dfc5c5d6069
Aaditya-G <aadityagupta400@gmail.com>
2023-10-10 02:08:39 +0530

    adds username to the textbox

--- components/chatContainer.js
+++ components/chatContainer.js
@@ -11,8 +11,9 @@ export default function ChatContainer({ messages }) {
   <li key={index} className={` ${message.isSent ? "sent" : "received"} ${message.isSent ? "flex flex-row items-center justify-end py-[3vh] max-md:py-[2vh] max-sm:py-[1vh]" : "flex flex-row items-center justify-start py-[3vh] max-md:py-[2vh] max-sm:py-[1vh]"}`}>
     {message.isSent ? (
       <>
-        <div className="pr-[1vw] w-[70%] max-sm:w-[60%]">
-          <div className="shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-white rounded-lg flex align-items-center justify-items-center py-[2vh] translate-y-[3vh] break-all">
+        <div className="pr-[1vw] w-[70%] max-sm:w-[60%] flex flex-col translate-y-[3vh]">
+          <div className="username justify-end text-right noir-pro max-sm:text-sm text-txt-mdg-username">zcross.mdg</div>
+          <div className="shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-white rounded-lg flex align-items-center justify-items-center py-[2vh]  break-all flex flex-col">
             <div className="mx-[1vw] text-txt-grey noir-pro max-sm:text-sm">{message.text}</div>
           </div>
         </div>
@@ -26,8 +27,9 @@ export default function ChatContainer({ messages }) {
       <Image src={Avatar} alt="" className="mr-2 max-w-[70%] max-h-full" />
       </div>
         
-        <div className="pl-[1vw] w-[70%] max-sm:w-[60%]">
-          <div className="shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-white rounded-lg flex align-items-center justify-items-center py-[2vh] translate-y-[3vh] break-all">
+        <div className="pl-[1vw] w-[70%] max-sm:w-[60%] flex flex-col translate-y-[3vh]">
+          <div className="noir-pro max-sm:text-sm text-txt-mdg-username">zcross.mdg</div>
+          <div className="shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-white rounded-lg flex align-items-center justify-items-center py-[2vh]  break-all">
             <div className="mx-[1vw] text-txt-grey noir-pro max-sm:text-sm">{message.text}</div>
           </div>
         </div>
