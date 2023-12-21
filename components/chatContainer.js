57ace530d6f4f8cc8d655be053c58d0c93afc8d2
Aaditya-G <aadityagupta400@gmail.com>
2023-12-21 11:23:05 +0530

    rfac: refactors chat container and fixes mobile width issue

--- components/chatContainer.js
+++ components/chatContainer.js
@@ -1,12 +1,11 @@
+// ChatContainer.js
+
 import React from "react";
 import Avatar from "../assets/avatar.svg"
 import Image from 'next/image'
 import moment from 'moment';
 
-
-
-
-export default function ChatContainer({ messages , messagesEndRef }) {
+export default function ChatContainer({ messages, messagesEndRef }) {
   
   const formatTime = (timestamp) => {
     const date = new Date(timestamp * 1000);
@@ -19,44 +18,23 @@ export default function ChatContainer({ messages , messagesEndRef }) {
     }
   }, [messages]);
 
-
-
   return (
     <div className="">
       <ul>
-      {messages.map((message, index) => (
-  <li key={index} className={` ${message.isSent ? "sent" : "received"} ${message.isSent ? "flex flex-row items-center justify-end py-[3vh] max-md:py-[2vh] max-sm:py-[1vh]" : "flex flex-row items-center justify-start py-[3vh] max-md:py-[2vh] max-sm:py-[1vh]"}`}>
-    {message.isSent ? (
-      <>
-        <div className="pr-[1vw] w-[70%] max-sm:w-[60%] flex flex-col translate-y-[3vh]">
-          <div className="username justify-end text-right noir-pro max-sm:text-sm text-txt-mdg-username">{message.username}</div>
-          <div className="shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-white rounded-lg flex align-items-center justify-items-center py-[2vh]  break-all flex flex-col">
-            <div className="mx-[1vw] text-txt-grey noir-pro max-sm:text-sm">{message.text}</div>
-            <div className="text-right absolute bottom-5 right-2 text-xs text-gray-500 pr-[1vw] pb-1 h-[0]">{formatTime(message.timestamp)}</div>
-          </div>
-        </div>
-        <div className="w-[6vw] max-md:w-[10vw] max-sm:w-[15vw]">
-      <Image src={Avatar} width ="80" height="80" alt="" className="ml-2 max-sm:w-[10vw] max-w-[5 0%] max-h-full" />
-      </div>
-      </>
-    ) : (
-      <>
-      <div className="w-[3vw]  max-md:w-[10vw] max-sm:w-[15vw]">
-      <Image src={message.avatar || Avatar} width ="70" height="70" alt="" className="rounded-full mr-2 max-sm:w-[10vw] max-w-[100%] max-h-full" />
-      </div>
-        
-        <div className="pl-[1vw] w-[70%] max-sm:w-[60%] flex flex-col translate-y-[3vh]">
-          <div className="noir-pro max-sm:text-sm text-txt-mdg-username">{message.username}</div>
-          <div className="shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-white rounded-lg flex align-items-center justify-items-center py-[2vh]  break-all">
-            <div className="mx-[1vw] text-txt-grey noir-pro max-sm:text-sm">{message.text}</div>
-            <div className="text-right absolute bottom-5 right-2 text-xs text-gray-500 pb-1 h-[0]">{formatTime(message.timestamp)}</div>
-          </div>
-        </div>
-      </>
-    )}
-  </li>
-))}
-
+        {messages.map((message, index) => (
+          <li key={index} className={`flex items-start ${message.isSent ? "justify-end" : "justify-start"} mb-4`}>
+            <div className={`relative flex ${message.isSent ? "flex-row-reverse" : ""}`}>
+              <div className="flex-shrink-0 w-12 h-12">
+                <Image src={message.avatar || Avatar} width="48" height="48" alt="" className="rounded-full" />
+              </div>
+              <div className={`bg-white rounded-lg shadow-[0px_4px_4px_rgba(0,0,0,0.25)] max-sm:w-[60vw] max-md:w-[50vw] max-lg:w-[40vw] max-2xl:w-[40vw] 2xl:max-w-[40vw] px-4 py-2 m-2 ${message.isSent ? "order-1" : "order-2"}`}>
+                <div className="text-txt-mdg-username">{message.username}</div>
+                <div className="text-txt-grey pt-2">{message.text}</div>
+                <div className={`text-xs text-gray-500 ${message.isSent ? "text-right" : "text-right"} mt-2`}>{formatTime(message.timestamp)}</div>
+              </div>
+            </div>
+          </li>
+        ))}
       </ul>
       <div ref={messagesEndRef} /> 
     </div>
