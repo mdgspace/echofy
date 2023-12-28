e0595e7f04200de6b80bbc09593b9d55b0ad6b27
Aaditya-G <aadityagupta400@gmail.com>
2023-12-28 18:35:59 +0530

    fix: makes login page responsive

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import bg from "../assets/bg.svg";
 // import { get, set } from './session-store';
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useRouter } from "next/navigation";
 import {
   getSessionUser,
@@ -16,6 +16,10 @@ import {
 export default function login() {
   const [username, setUsername] = useState("");
   const router = useRouter();
+
+  useEffect(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  }, []);
 
   function handleUsernameChange(event) {
     setUsername(event.target.value);
@@ -59,45 +63,54 @@ export default function login() {
   }
 
   return (
-    <div className="flex justify-center items-center h-full bg-[url('../assets/bg.svg')] bg-auto w-full h-screen bg-contain">
-      <div className="flex flex-col space-y-33 m-60 justify-center text-center">
-        <div className="w-[696px] h-[535px] shrink-0 opacity-90 shadow-[0px_4px_24px_-1px_rgba(0,0,0,0.25)] backdrop-blur-[20px] rounded-[20px] ">
-          <div className=" h-[115px] noir-pro text-center mt-[65px]">
-            <div className="text-bg-orange text-opacity-60 ">LOGIN</div>
-          </div>
-          <div className="h-[60px] outfit text-center text-bg-orange text-opacity-60 p-50 mt-[40px]">
-            .mdg
-          </div>
+   <div className="flex flex-col justify-between items-center bg-[url('../assets/bg.svg')] bg-cover w-full h-screen">
+  <div className="flex flex-col justify-center flex-grow text-center max-sm:w-[80vw] max-md:w-[70vw] max-lg:w-[60vw] max-xl:w-[50vw] 2xl:w-[25vw]">
+    <div className="opacity-80 shadow-xl backdrop-blur-md rounded-2xl p-4 sm:p-6 md:p-8">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl noir-pro-bold text-orange-600 mb-4">LOGIN</h1>
+      <p className="text-orange-600 text-opacity-60 noir-pro text-sm sm:text-base md:text-lg mb-4">.mdg</p>
 
-          <div className="w-[577px] h-[66px] shrink-0 shadow-[0px_4px_7px_0px_rgba(0,0,0,0.20)] rounded-[10px] mt-[42px] ml-[60px]">
-            <div className="flex justify-center items-center h-full p-50">
-              <div className=" bg-transparent">
-                <input
-                  type="text"
-                  placeholder="Username"
-                  value={username}
-                  onChange={handleUsernameChange}
-                  onKeyDown={handleEnterClick}
-                />
-              </div>
-            </div>
-          </div>
+      <div className="shadow-md rounded-md my-4 mx-6">
+        <input
+          type="text"
+          placeholder="Username"
+          className="w-full p-3 rounded-md text-gray-700"
+          value={username}
+          onChange={handleUsernameChange}
+          onKeyDown={handleEnterClick}
+        />
+      </div>
 
-          <div className="w-[238px] h-[66px] shrink-0 rounded-[10px] bg-white ml-[60px] mt-[40px] cursor-pointer">
-            <div
-              className="text-bg-orange noir-pro-small text-center  "
-              onClick={handleChatWithUsClick}
-            >
-              Chat with Us
-            </div>
-            <div className="w-[319px] h-[66px] shrink-0 rounded-[10px] bg-bg-orange ml-[300px]  -mr-[100px] -mt-[40px]">
-              <div className="text-white noir-pro-small text-center flex flex-col justify-end  ">
-                FAQs
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="flex flex-row sm:flex-row justify-center items-center gap-4 mt-6 ">
+        <button
+          className="rounded-xl bg-white py-3 px-6 text-orange-600 noir-pro-small font-medium max-sm:text-xs"
+          onClick={handleChatWithUsClick}
+        >
+          Chat with Us
+        </button>
+        <button
+          className="rounded-xl bg-orange-600 py-3 px-6 text-white font-medium max-sm:text-xs"
+          onClick={handleFAQsClick}
+        >
+          FAQs
+        </button>
       </div>
     </div>
+  </div>
+  <div className="flex flex-row gap-2 mt-5">
+    <div className="text-center font-medium noir-pro text-xl max-sm:text-base max-md:text-lg max-xl:text-lg">
+      Made with ❤️ by 
+    </div>
+    <a 
+      href="https://mdgspace.org"
+      target="_blank" 
+      rel="noopener noreferrer" 
+      className="noir-pro-bold text-xl max-sm:text-base max-md:text-lg max-xl:text-lg text-orange-600"
+    >
+    MDGSpace
+    </a>
+  </div>
+  
+</div>
+
   );
 }
