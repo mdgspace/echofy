11a7f9ae8bc9f31c19fad73a2ff8d02eacc606f1
the-geek-deep <deepumrajkar15@gmail.com>
2024-04-03 01:59:30 +0530

    fix: reverts changes from prev commits

--- backend/db/client.go
+++ backend/db/client.go
@@ -24,7 +24,7 @@ func Init() {
 func redisInit(portNumber, dbNumber int, password string) {
 	ctx = context.Background()
 	redisClient = redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("redis%v", portNumber), //port number can be changed as per our wish
+		Addr:     fmt.Sprintf("redis:%v", portNumber), //port number can be changed as per our wish
 		Password: password,
 		DB:       dbNumber,
 	})
@@ -353,23 +353,23 @@ func AddUserInfoToDb(username string, userId string, userAgent string, ip string
 }
 
 func GetUserInfo(userId string) string {
-	info, _ := redisClient.Get(ctx, fmt.Sprintf("info:%v", userId)).Result()
-	var _info models.UserInfo
-	err := json.Unmarshal([]byte(info), &_info)
-	if err != nil {
+	info, _ := redisClient.Get(ctx, fmt.Sprintf("info:%v", userId)).Result()	
+    var _info models.UserInfo
+    err := json.Unmarshal([]byte(info), &_info)
+	if(err != nil){
 		logging.LogException(err)
 		panic(err)
 	}
 	formattedInfo := fmt.Sprintf(
-		"UserID: %s\nName: %s\nIP: %s\nLocation: %s\nOS: %s\nAgent: %s\nChatChannel: %s",
-		_info.UserID,
-		_info.Username,
-		_info.IP,
-		_info.Location,
-		_info.OS,
-		_info.Agent,
-		_info.Channel,
-	)
+        "UserID: %s\nName: %s\nIP: %s\nLocation: %s\nOS: %s\nAgent: %s\nChatChannel: %s",
+        _info.UserID,
+        _info.Username,
+        _info.IP,
+        _info.Location,
+        _info.OS,
+        _info.Agent,
+        _info.Channel,
+    )
 	return formattedInfo
 }
 
