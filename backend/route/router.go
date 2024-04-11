757cffb25b7b54fea19b65eee8f84e290edd2030
Aaditya Gupta <aadityagupta400@gmail.com>
2024-04-11 23:00:07 +0530

    feat: add cors to backend

--- backend/route/router.go
+++ backend/route/router.go
@@ -5,11 +5,17 @@ import (
 
 	sentryecho "github.com/getsentry/sentry-go/echo"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func Init() {
 	e := echo.New()
 	e.Use(sentryecho.New(sentryecho.Options{}))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000" , "https://mdgspace.org"},
+		AllowMethods: []string{"GET", "POST"},
+		AllowHeaders: []string{"Content-Type"},
+	}))
 	e.GET("/chat", api.JoinChat())
 	e.GET("/projects", api.GetProjects())
 	e.POST("/userInfo", api.ReceivedFrontendUserInfo())
