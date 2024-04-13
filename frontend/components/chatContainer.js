67460a2bf0c6ee33d0e02eced99c1772e71343b4
Amey <ameykhandalkar5921@gmail.com>
2024-04-14 01:27:53 +0530

    feat: adds avatar randomization

--- frontend/components/chatContainer.js
+++ frontend/components/chatContainer.js
@@ -1,9 +1,28 @@
 // ChatContainer.js
 
 import React from "react";
-import Avatar from "../assets/avatar.svg"
 import Image from 'next/image'
 import moment from 'moment';
+import { useState } from "react"; 
+
+import Avatar1 from "../assets/avatars/avatar_1.svg"
+import Avatar2 from "../assets/avatars/avatar_2.svg"
+import Avatar3 from "../assets/avatars/avatar_3.svg"
+import Avatar4 from "../assets/avatars/avatar_4.svg"
+import Avatar5 from "../assets/avatars/avatar_5.svg"
+import Avatar6 from "../assets/avatars/avatar_6.svg"
+import Avatar7 from "../assets/avatars/avatar_7.svg"
+import Avatar8 from "../assets/avatars/avatar_8.svg"
+import Avatar9 from "../assets/avatars/avatar_9.svg"
+import Avatar10 from "../assets/avatars/avatar_10.svg"
+import Avatar11 from "../assets/avatars/avatar_11.svg"
+import Avatar12 from "../assets/avatars/avatar_12.svg"
+import Avatar13 from "../assets/avatars/avatar_13.svg"
+import Avatar14 from "../assets/avatars/avatar_14.svg"
+import Avatar15 from "../assets/avatars/avatar_15.svg"
+
+
+import {  getAvatar } from "../services/utilities/utilities";
 
 export default function ChatContainer({ messages, messagesEndRef }) {
   const formatTime = (timestamp) => {
@@ -11,10 +30,21 @@ export default function ChatContainer({ messages, messagesEndRef }) {
     return moment(date).format('hh:mm A');
   };
 
+  const AvatarList = [Avatar1, Avatar2, Avatar3, Avatar4, Avatar5, Avatar6, Avatar7, Avatar8, Avatar9, Avatar10, Avatar11, Avatar12, Avatar13, Avatar14, Avatar15] 
+  const [Avatar, setAvatar] = useState(Avatar1)
+
   React.useEffect(() => {
     if (messagesEndRef.current) {
       messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
+    async function loadAvatar() {
+      const AvatarId = await getAvatar()
+      const Avatar = AvatarList[AvatarId]
+      setAvatar(Avatar)
+    }
+    loadAvatar();
+
+
   }, [messages]);
 
   return (
