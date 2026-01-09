3b055fdb2afe07d1433221c259b3b9839b023551
Budhayan Chakraborty <budhayanc2005@gmail.com>
2026-01-10 02:56:59 +0530

    feat: Integrate with Echofy Chatbot

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,29 +4,27 @@ import { useState } from "react";
 import ChatBot from "../assets/chatbot.svg";
 import SlackLogo from "../assets/slack.svg";
 import LoginModal from "../components/chat/loginModal";
-import ChatBotLoginModal from "../components/chatbot/chatbotLoginModal";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [isChatBotModalOpen, setISChatBotModalOpen] = useState<boolean>(false);
   const [redirect, setRedirect] = useState<string>("");
+  const router = useRouter();
 
-  const openModal = (redirect:string) => {
+  const goToChatbot = () => {
+    router.push("/chat_bot");
+  };
+
+  const openModal = (redirect: string) => {
     setIsModalOpen(true);
     setRedirect(redirect);
   };
 
-  const openChatBotModal = () => {
-    setISChatBotModalOpen(true);
-  };
 
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
-  const closeChatBotLoginModal = () => {
-    setISChatBotModalOpen(false);
-  };
 
   const goToPublicChat = () => {
     openModal("public");
@@ -36,23 +34,17 @@ export default function Home() {
     openModal("private");
   };
 
-  const goToChatbot = () => {
-    openChatBotModal();
-  };
   return (
     <>
       <div className="main bg-white w-full bg-contain">
         <div className="grid grid-cols-24 w-full mt-2 h-[95vh]">
           <div className="flex flex-col items-center col-span-7 bg-white max-md:hidden">
             <div className="flex flex-col items-center p-2 bg-white-primary rounded-xl w-[95%]">
-              <Box channel="public"/>
+              <Box channel="public" />
             </div>
           </div>
           {isModalOpen && (
             <LoginModal onClose={closeModal} redirect={redirect} />
-          )}
-          {isChatBotModalOpen && (
-            <ChatBotLoginModal onClose={closeChatBotLoginModal} redirect={redirect} />
           )}
           <div className="col-span-17 flex flex-col justify-center items-center bg-light-grey max-md:col-span-24 rounded-xl mr-[1vw]">
             <div className="w-1/2">
@@ -71,26 +63,25 @@ export default function Home() {
                 </div>
               </div>
               <div className="flex flex-col justify-center items-center gap-4 mt-8">
-              <div
-                  className="flex items-center justify-center py-4 w-full px-8 rounded-full bg-customBlue text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-customBlue focus:ring-opacity-50 hover:cursor-not-allowed relative group"
-                  // onClick={goToChatbot}
+                <div
+                  className="flex items-center justify-center py-4 w-full  px-8 rounded-full bg-customBlue text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-customBlue focus:ring-opacity-50 hover:cursor-pointer"
+                  onClick={goToChatbot}
                 >
                   <p className="font-Roboto font-medium text-xl tracking-tighter flex gap-2">
                     <Image alt={"chat bot"} src={ChatBot} />
                     TALK TO OUR CHATBOT
                   </p>
-
-                  {/* Tooltip for coming soon message */}
-                  <div className="absolute bottom-full mb-2 hidden group-hover:flex bg-gray-700 text-white text-sm px-3 py-1 rounded whitespace-nowrap">
-                    Feature coming soon!
-                  </div>
                 </div>
                 <div
                   className="flex items-center justify-center py-4 w-full  px-8 rounded-full bg-customBlue text-white hover:bg-blue-600 hover:cursor-pointer  focus:outline-none focus:ring-2 focus:ring-customBlue focus:ring-opacity-50"
                   onClick={goToPrivateChat}
                 >
                   <p className="font-Roboto font-medium text-xl tracking-tighter flex gap-2">
-                    <Image alt={"slack logo"} src={SlackLogo} className="text-sky-400" />
+                    <Image
+                      alt={"slack logo"}
+                      src={SlackLogo}
+                      className="text-sky-400"
+                    />
                     PRIVATE CHAT ON SLACK
                   </p>
                 </div>
